2dfddf22a45bcfba5f7199dae10a6d29fb0abf2c
Ernesto <elgn@cin.ufpe.br>
2023-03-30 17:50:57 -0300

    criação de título da colunas do topicsboard

--- eye-chat/eye-chat/UI/Components/TopicsBoardComponent.swift
+++ eye-chat/eye-chat/UI/Components/TopicsBoardComponent.swift
@@ -12,29 +12,47 @@ struct TopicsBoardComponent: View {
         Grid() {
             GridRow{
                 Grid{
+                    ZStack{
+                        Text("Respostas Rápidas")
+                            .padding()
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .cornerRadius(20)
+                    } .frame(maxWidth: 310.22, maxHeight: 35)
+                        .background(Color.blue)
+                        .cornerRadius(120)
+
                     TopicsCardComponent(style: .no)
                     TopicsCardComponent(style: .yes)
                     TopicsCardComponent(style: .maybe)
                 }
                 
                 Grid{
+                    Text("Tópicos")
+                        .font(.system(size: 18))
+
                     TopicsCardComponent(style: .family)
                     TopicsCardComponent(style: .entertainment)
                     TopicsCardComponent(style: .health)
                     
                 }
                 Grid{
+                    Text("Perguntas")
+
                     TopicsCardComponent(style: .story)
                     TopicsCardComponent(style: .day)
                     TopicsCardComponent(style: .sorry)
                 }
+                
+               
+                    }
+                }
             }
         }
-    }
-}
-    
-    struct TopicsBoardComponent_Previews: PreviewProvider {
-        static var previews: some View {
-            TopicsBoardComponent()
+        
+        struct TopicsBoardComponent_Previews: PreviewProvider {
+            static var previews: some View {
+                TopicsBoardComponent()
+            }
         }
-    }
+ 
