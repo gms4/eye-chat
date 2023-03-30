b8d69d1a893a950a31a3d964add2155f8c20d1a3
Ernesto <elgn@cin.ufpe.br>
2023-03-30 15:47:54 -0300

    Construindo Cards de tópicos e frases

---
+++ eye-chat/eye-chat/UI/Components/TopicCardComponent.swift
@@ -0,0 +1,20 @@
+//
+//  TopicCardComponent.swift
+//  eye-chat
+//
+//  Created by egln on 30/03/23.
+//
+//
+import SwiftUI
+
+struct TopicCardComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopicCardComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        TopicCardComponent()
+    }
+}
