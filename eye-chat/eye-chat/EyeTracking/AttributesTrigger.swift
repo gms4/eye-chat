050be8eb449272e4d04c92c4491b5a140717af7e
Alex Damascena <apfd@cin.ufpe.br>
2023-04-03 18:58:06 -0300

    feature(integration): add tempalate for eye tracking

---
+++ eye-chat/eye-chat/EyeTracking/AttributesTrigger.swift
@@ -0,0 +1,19 @@
+//
+//  AttributesTrigger.swift
+//  eye-chat
+//
+//  Created by alexdamascena on 03/04/23.
+//
+
+import Foundation
+
+
+struct AttributesTrigger {
+    
+    var axisXFirstSectionTrigger: [Function]?
+    var axisXSecondSectionTrigger: [Function]?
+    var upTrigger: Function?
+    var downTrigger: Function?
+    var goBack: Function?
+    
+}
