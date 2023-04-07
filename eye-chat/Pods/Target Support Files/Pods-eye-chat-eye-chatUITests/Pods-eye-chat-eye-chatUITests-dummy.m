b4130cf66871b3213e75b3c8e593eff2c847ae00
Alex Damascena <apfd@cin.ufpe.br>
2023-04-07 19:04:08 -0300

    feature(integration): remove pod dependency to merge

--- eye-chat/Pods/Target Support Files/Pods-eye-chat-eye-chatUITests/Pods-eye-chat-eye-chatUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_eye_chat_eye_chatUITests : NSObject
-@end
-@implementation PodsDummy_Pods_eye_chat_eye_chatUITests
-@end
--- eye-chat/Pods/Target Support Files/Pods-eye-chat/Pods-eye-chat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_eye_chat : NSObject
-@end
-@implementation PodsDummy_Pods_eye_chat
-@end
