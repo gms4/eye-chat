3de4ba41adb12464a22db15dde41e118816612f7
Alex Damascena <apfd@cin.ufpe.br>
2023-04-07 11:07:24 -0300

    feature(video): create signalingClient+WebSocketProviderDelegate

---
+++ eye-chat/eye-chat/Services/SignalingClient+WebSocketProviderDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SignalingClient+WebSocketProviderDelegate.swift
+//  eye-chat
+//
+//  Created by alexdamascena on 07/04/23.
+//
+
+import Foundation
+
+
+extension SignalingClient: WebSocketProviderDelegate {
+    func webSocketDidConnect(_ webSocket: WebSocketProvider) {
+        self.delegate?.signalClientDidConnect(self)
+    }
+    
+    func webSocketDidDisconnect(_ webSocket: WebSocketProvider) {
+        self.delegate?.signalClientDidDisconnect(self)
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            debugPrint("Trying to reconnect")
+            self.webSocket.connect()
+        }
+    }
+    
+    func webSocket(_ webSocket: WebSocketProvider, didReceiveData data: Data) {
+        let message: Message
+        do {
+            message = try self.decoder.decode(Message.self, from: data)
+        }
+        catch {
+            debugPrint("Warning: Could not decode incoming message: \(error)")
+            return
+        }
+        
+        switch message {
+        case .candidate(let iceCandidate):
+            self.delegate?.signalClient(self, didReceiveCandidate: iceCandidate.rtcIceCandidate)
+        case .sdp(let sessionDescription):
+            self.delegate?.signalClient(self, didReceiveRemoteSdp: sessionDescription.rtcSessionDescription)
+        }
+
+    }
+}
